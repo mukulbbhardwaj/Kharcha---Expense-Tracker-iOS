aa20bcb1abd7b025194f634407911bab2190aff1
Mukul Bhardwaj <themukulbhardwaj@gmail.com>
2022-03-27 23:44:45 +0530

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Mukul Bhardwaj on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

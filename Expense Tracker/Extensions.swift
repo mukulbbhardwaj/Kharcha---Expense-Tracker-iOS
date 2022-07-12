abf3b3dc13c19037fa09ef29177b08270b5f7ab3
Mukul Bhardwaj <themukulbhardwaj@gmail.com>
2022-07-13 01:12:47 +0530

    Added Extensions file for Color

---
+++ Expense Tracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Expense Tracker
+//
+//  Created by Mukul Bhardwaj on 13/07/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
